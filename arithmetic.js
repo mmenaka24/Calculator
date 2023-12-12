66639123065e4f52e12b6557c5eb3519252bfad8
mmenaka24 <menaka_maruthainar@live.com>
2023-12-12 18:20:41 +0000

    Added try catch for valid operator

--- arithmetic.js
+++ arithmetic.js
@@ -1,5 +1,15 @@
 const userInput = require('./userInput');
 
+const operatorErrorMessage = `Sorry, that is not a valid operator.
+        
+This calculator can perform the following operations:
+Addition: please enter +, plus, add, addition or sum
+Subtraction: please enter -, minus, subtract, subtraction or difference
+Multiplication: please enter *, x, ., multiply, multiplication or product
+Division: please enter /, divide, division or quotient
+
+Please run the program again with one of these operations :)`
+
 const setOperationAndCheckValidity = (operator) => {
     if (operator == "+" || operator == "plus" || operator == "add" || operator == "addition" || operator == "sum") {
         return "add";
@@ -9,18 +19,7 @@ const setOperationAndCheckValidity = (operator) => {
         return "multiply";
     } else if (operator == "/" || operator == "divide" || operator == "division" || operator == "quotient") {
         return "divide";
-    } else {
-        console.log(`Sorry, that is not a valid operator.
-        
-        This calculator can perform the following operations:
-        Addition: please enter +, plus, add, addition or sum
-        Subtraction: please enter -, minus, subtract, subtraction or difference
-        Multiplication: please enter *, x, ., multiply, multiplication or product
-        Division: please enter /, divide, division or quotient
-        
-        Please run the program again with one of these operations :)`);
-        process.exit(0);
-    }
+    } else throw new Error(operatorErrorMessage);
 }
 
 const createArrayOfNumbers = (iterations) => {
@@ -42,47 +41,51 @@ const createArrayOfNumbers = (iterations) => {
 }
 
 exports.performOneArithmeticCalculation = function() {
-    //first get operator and check that it is valid
-    const operator = userInput.getStringInputWithPrompt('Please enter the operator:');
-    let operation = setOperationAndCheckValidity(operator);
+    try {
+        //first get operator and check that it is valid
+        const operator = userInput.getStringInputWithPrompt('Please enter the operator:');
+        let operation = setOperationAndCheckValidity(operator);
 
-    //ask how many numbers
-    const iterations = userInput.getNumberInputWithPrompt(`How many numbers do you want to ${operation}?`);
+        //ask how many numbers
+        const iterations = userInput.getNumberInputWithPrompt(`How many numbers do you want to ${operation}?`);
 
-    //now get the numbers
-    let arr = createArrayOfNumbers(iterations);
+        //now get the numbers
+        let arr = createArrayOfNumbers(iterations);
 
-    //line of code to clarify numbers and operation
-    console.log(`Your numbers are ${arr} and you want to ${operation} them...`);
+        //line of code to clarify numbers and operation
+        console.log(`Your numbers are ${arr} and you want to ${operation} them...`);
 
-    //remove zeros from array if dividing
-    if (operation === "divide") {
-        const firstElement = arr[0];
-        arr = [firstElement].concat(arr.slice(1).filter((number) => number !== 0));
-    }
-
-    console.log(arr);
-
-    //now perform the relevant operation
-
-    const answer = arr.reduce(
-        (accumulator, currentValue) => {
-            switch (operation) {
-                case "add":
-                    return (accumulator + currentValue);
-                case "subtract":
-                    return (accumulator - currentValue);
-                case "multiply":
-                    return (accumulator * currentValue);
-                case "divide":
-                    return (accumulator / currentValue);
-                default:
-                    console.log('Sorry, something seems to have gone wrong. Try again?');
-            }
+        //remove zeros from array if dividing
+        if (operation === "divide") {
+            const firstElement = arr[0];
+            arr = [firstElement].concat(arr.slice(1).filter((number) => number !== 0));
         }
-    );
 
-    //finally, print the answer
-    console.log(`
-    The answer is ${answer}`);
+        console.log(arr);
+
+        //now perform the relevant operation
+
+        const answer = arr.reduce(
+            (accumulator, currentValue) => {
+                switch (operation) {
+                    case "add":
+                        return (accumulator + currentValue);
+                    case "subtract":
+                        return (accumulator - currentValue);
+                    case "multiply":
+                        return (accumulator * currentValue);
+                    case "divide":
+                        return (accumulator / currentValue);
+                    default:
+                        console.log('Sorry, something seems to have gone wrong. Try again?');
+                }
+            }
+        );
+
+        //finally, print the answer
+        console.log(`
+        The answer is ${answer}`);
+    } catch (e) {
+        console.error(e.message);
+    }
 }
